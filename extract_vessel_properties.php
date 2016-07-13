def1236c4242e10aad341c63c644707089ded4d7
Angelica Lo Duca <angelica@MacBook-Pro-di-Angelica.local>
2016-07-13 15:57:34 +0200

    added config file

--- extract_vessel_properties.php
+++ extract_vessel_properties.php
@@ -19,6 +19,7 @@
 // These scripts are contained in the repository templates (in a separate Git)
 include('../templates/php/utilities/remote.php');
 include('../templates/php/utilities/utilities.php');
+include('utilities/config.php'); // configuration file, not included in Git
 
 $basic_url = "https://www.vesselfinder.com/vessels/";
 
@@ -29,16 +30,16 @@ $vXPATH = "//span[@class=\"small-7 columns value\"]";
 $ev = array("Premium users only");
 $en = array("Last report:","Builder","Place of build","Deadweight","Owner","Manager");
 
-$conn = mysqlconnect("root", NULL,"OSIRIS");
-$query = "SELECT DISTINCT(MMSI), NAME, IMO FROM AIS_Samples WHERE NAME != ''";
+//$conn = mysqlconnect("root", NULL,"OSIRIS");
+$query = "SELECT DISTINCT(MMSI), NAME FROM Bosforo WHERE NAME != ''";
 $qr = mysqli_query($conn, $query);
 if($qr != false && mysqli_num_rows($qr) > 0)
 	while($row = mysqli_fetch_assoc($qr))
 	{
 		$name = str_replace(" ","-",preg_replace('/\s+/', ' ',str_replace('-'," ",str_replace(array('/',"'","..."),"",trim($row['NAME'])))));
-		$imo = $row['IMO'];
+		//$imo = $row['IMO'];
 		$mmsi = $row['MMSI'];
-		$vessel = "$name-IMO-$imo-MMSI-$mmsi";
+		$vessel = "$name-IMO-0-MMSI-$mmsi";
 		$vessel_url = $basic_url.$vessel;
 		
 		echo $vessel_url."\n";
@@ -69,7 +70,7 @@ if($qr != false && mysqli_num_rows($qr) > 0)
 			$spn = substr($spn,0, strlen($spn)-1);
 			$spv = substr($spv,0, strlen($spv)-1);
 			
-			$iq = "INSERT INTO AIS_Info(`MMSI`,$spn) VALUES ('$mmsi',$spv)";
+			$iq = "INSERT INTO Bosforo_Info(`MMSI`,$spn) VALUES ('$mmsi',$spv)";
 			mysqli_query($conn,$iq);
 		}
 	}
---
+++ utilities/config.php
@@ -0,0 +1,9 @@
+<?php 
+
+require_once('../../templates/php/utilities/utilities.php');
+
+$user = "root";
+$password = "";
+$conn = mysqlconnect($user, $password);
+
+?>
