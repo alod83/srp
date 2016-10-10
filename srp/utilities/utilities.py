b640724b7ba60fe5f3f005b83ae20f4dd8d5301a
Angelica Lo Duca <angelica@MacBook-Pro-di-Angelica.local>
2016-10-10 23:50:59 +0200

    removed unused file

--- srp/utilities/utilities.py
+++
@@ -1,8 +0,0 @@
-# This script contains a collection of functions
-
-
-# This function runs a mysql query
-def myquery(cnx,query,buffered=True):
-    cursor = cnx.cursor(buffered)
-    cursor.execute(query)
-    return cursor
