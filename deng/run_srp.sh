dbeaa366de9244548ea5c3adc7599395bd2edee6
angelica lo duca <angelica@osiris1.internal>
2017-08-03 18:04:25 +0200

    deleted some folders

--- deng/run_srp.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-echo "* * * * * WELCOME TO SHIP ROUTE PREDICTION ALGORITHM * * * * *"
-echo "Please look at config.ini for configuration options"
-
-cd discretize
-sh run_discretize.sh
-cd ..
